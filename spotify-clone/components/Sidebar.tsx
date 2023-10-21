143c361c72d0dc1fdd561c0ee969d2efc8f5dd83
Dorothea Lim Hui Ying <Dorothea@Internationals-MacBook-Pro.local>
2023-10-21 09:56:50 +0800

    environment setup complete

---
+++ spotify-clone/components/Sidebar.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+//fix typscript error
+interface SidebarProps {
+    children: React.ReactNode;
+}
+
+const Sidebar: React.FC<SidebarProps> = ({
+    children
+}) => {
+    return (
+        <div>
+            {children}
+        </div>
+     );
+}
+ 
+export default Sidebar;
