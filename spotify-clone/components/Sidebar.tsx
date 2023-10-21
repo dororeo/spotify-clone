021c2edda097cf444aa3105feae0fd535b461bc8
Dorothea Lim Hui Ying <Dorothea@Internationals-MacBook-Pro.local>
2023-10-21 10:48:54 +0800

    created box component

--- spotify-clone/components/Sidebar.tsx
+++ spotify-clone/components/Sidebar.tsx
@@ -1,4 +1,12 @@
-import React from "react";
+// import React from "react";
+"use client";
+
+import { usePathname } from "next/navigation";
+import { useMemo } from "react";
+import { HiHome } from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
+import Box from "./Box";
+
 
 //fix typscript error
 interface SidebarProps {
@@ -8,9 +16,43 @@ interface SidebarProps {
 const Sidebar: React.FC<SidebarProps> = ({
     children
 }) => {
+    //add hook
+    const pathname = usePathname();
+
+    //array of possible routes
+    const routes = useMemo(() => [
+        {
+            icon: HiHome,
+            label: 'Home',
+            active: pathname !== '/search', //make it active everytime the path name is not search
+            href: '/',
+        },
+        {
+            icon: BiSearch,
+            label: 'Search',
+            active: pathname === '/search',
+            href: '/search',
+        }
+    ], [pathname]);
+
     return (
-        <div>
-            {children}
+        <div className="flex h-full">
+            <div 
+                className="
+                    hidden
+                    md:flex
+                    flex-col
+                    gap-y-2
+                    bg-black
+                    h-full
+                    w-[300px]
+                    p-2
+                "
+            >
+                <Box>
+                    Sidebar Navigation
+                </Box>
+            </div>
         </div>
      );
 }
