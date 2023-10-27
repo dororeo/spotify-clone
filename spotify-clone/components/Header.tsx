5c153808a9aad70b81b33bae2b4a724dfef0586e
Dorothea Lim Hui Ying <Dorothea@Internationals-MacBook-Pro.local>
2023-10-27 13:32:31 +0800

    successfully create user and logged in

--- spotify-clone/components/Header.tsx
+++ spotify-clone/components/Header.tsx
@@ -7,6 +7,7 @@ import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
 import Button from "./Button";
+import useAuthModal from "@/hooks/useAuthModal";
 
 interface HeaderProps {
     children: React.ReactNode;
@@ -17,6 +18,8 @@ const Header: React.FC<HeaderProps> = ({
     children,
     className
 }) => {
+
+    const authModal = useAuthModal();
 
     const router = useRouter();
 
@@ -62,12 +65,12 @@ const Header: React.FC<HeaderProps> = ({
             <div className="flex justify-between items-center gap-x-4">
                 <>
                 <div>
-                    <Button onClick={() => {}} className="bg-transparent text-neutral-300 font-medium">
+                    <Button onClick={authModal.onOpen} className="bg-transparent text-neutral-300 font-medium">
                         Sign Up
                     </Button>
                 </div>
                 <div>
-                    <Button onClick={() => {}} className="bg-white px-6 py-2">
+                    <Button onClick={authModal.onOpen} className="bg-white px-6 py-2">
                         Log In
                     </Button>
                 </div>
