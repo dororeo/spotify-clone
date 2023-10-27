00773a8d4fca732741f3f0f0cebf15a5ccd41799
Dorothea Lim Hui Ying <Dorothea@Internationals-MacBook-Pro.local>
2023-10-27 13:42:26 +0800

    handle logout

--- spotify-clone/components/Header.tsx
+++ spotify-clone/components/Header.tsx
@@ -8,6 +8,9 @@ import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
 import Button from "./Button";
 import useAuthModal from "@/hooks/useAuthModal";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import { useUser } from "@/hooks/useUser";
+import { FaUserAlt } from "react-icons/fa";
 
 interface HeaderProps {
     children: React.ReactNode;
@@ -20,66 +23,92 @@ const Header: React.FC<HeaderProps> = ({
 }) => {
 
     const authModal = useAuthModal();
-
     const router = useRouter();
 
-    const handleLogout = () => {
-        //handle logout 
+    const supabaseClient = useSupabaseClient();
+    //extract user from useUser hook
+    const { user } = useUser();
+
+
+    const handleLogout = async () => {
+        const { error } = await supabaseClient.auth.signOut();
+
+        //to do: reset playing songs
+
+        router.refresh();
+
+        if (error) {
+            console.log(error);
+        }
     }
 
-    return ( 
+    return (
         <div className={twMerge(`
             h-fit
             bg-gradient-to-b
             from-emerald-800
             p-6
         `, className)}>
-        <div className="
+            <div className="
         w-full
         mb-4
         flex
         items-center
         justify-between
         ">
-            <div className="
+                <div className="
             hidden
             md:flex
             gap-x-2
             items-center
             ">
-                <button onClick={() => router.back()} className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition">
-                    <RxCaretLeft size={35} className="text-white"/>
-                </button>
-                <button onClick={() => router.forward()} className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition">
-                    <RxCaretRight size={35} className="text-white"/>
-                </button>
-            </div>
-            <div className="flex md:hidden gap-x-2 items-center">
-                <button className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition">
-                    <HiHome size={20} className="text-black"/>
-                </button>
-                <button className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition">
-                    <BiSearch size={20} className="text-black"/>
-                </button>
-            </div>
-            <div className="flex justify-between items-center gap-x-4">
-                <>
-                <div>
-                    <Button onClick={authModal.onOpen} className="bg-transparent text-neutral-300 font-medium">
-                        Sign Up
-                    </Button>
+                    <button onClick={() => router.back()} className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition">
+                        <RxCaretLeft size={35} className="text-white" />
+                    </button>
+                    <button onClick={() => router.forward()} className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition">
+                        <RxCaretRight size={35} className="text-white" />
+                    </button>
                 </div>
-                <div>
-                    <Button onClick={authModal.onOpen} className="bg-white px-6 py-2">
-                        Log In
-                    </Button>
+                <div className="flex md:hidden gap-x-2 items-center">
+                    <button className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition">
+                        <HiHome size={20} className="text-black" />
+                    </button>
+                    <button className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition">
+                        <BiSearch size={20} className="text-black" />
+                    </button>
                 </div>
-                </>
+                <div className="flex justify-between items-center gap-x-4">
+                    {user ? (
+                        <div
+                        className="flex gap-x-4 items-center">
+                            <Button
+                            onClick={handleLogout} className="bg-white px-6 py-2">
+                            Logout
+                            </Button>
+                            <Button onClick={() => router.push('/account')}
+                            className="bg-white">
+                                <FaUserAlt />
+                            </Button>
+                        </div>
+                    ) : (
+                        <>
+                            <div>
+                                <Button onClick={authModal.onOpen} className="bg-transparent text-neutral-300 font-medium">
+                                    Sign Up
+                                </Button>
+                            </div>
+                            <div>
+                                <Button onClick={authModal.onOpen} className="bg-white px-6 py-2">
+                                    Log In
+                                </Button>
+                            </div>
+                        </>
+                    )}
+                </div>
             </div>
+            {children}
         </div>
-        {children}
-        </div>
-     );
+    );
 }
- 
+
 export default Header;
