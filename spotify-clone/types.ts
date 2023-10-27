69833e82c652a45cb28cb66df64b1385a102f315
Dorothea Lim Hui Ying <Dorothea@Internationals-MacBook-Pro.local>
2023-10-27 21:46:46 +0800

    load songs on page

--- spotify-clone/types.ts
+++ spotify-clone/types.ts
@@ -1,6 +1,16 @@
 import Stripe from "stripe";
 
 //models
+
+export interface Song {
+    id: string;
+    user_id: string;
+    artist: string;
+    title: string;
+    song_path: string;
+    image_path: string;
+};
+
 export interface UserDetails {
   id: string;
   first_name: string;
